b60486f1d1b4daf921f01841a2d33c96c172ea0f
HIRU-VIRU <xmyhiruthik2020@gmail.com>
2024-04-21 20:42:46 +0530

    first commit

--- LU.py
+++ LU.py
@@ -1,14 +1,24 @@
-# To print L and U matrix
+'''
+Program to find L and U matrix using LU decomposition.
+Developed by: HIRUTHIK SUDHAKAR
+RegisterNumber: 212223240054
+'''
 import numpy as np
-#add the library to import lu
-A = np.array(eval(input()))
-#add the code to get the L and U matrix
+from scipy.linalg import lu
+A=np.array(eval(input()))
+P,L,U=lu(A)
+print(L)
+print(U)
 
-
+'''Program to solve a matrix using LU decomposition.
+Developed by: HIRUTHIK SUDHAKAR
+RegisterNumber: 212223240054
+'''
 # To print X matrix (solution to the equations)
 import numpy as np
-#add the library to import lu_factor, lu_solve
-A = np.array(eval(input()))
-b = np.array(eval(input()))
-#add the code to get the solution of the matrix
-
+from scipy.linalg import lu_factor,lu_solve
+A=np.array(eval(input()))
+B=np.array(eval(input()))
+lu,piv=lu_factor(A)
+x=lu_solve((lu,piv),B)
+print(x)
